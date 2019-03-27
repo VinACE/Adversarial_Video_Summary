d4cceb8085c1b95dc04af09c24fd4e642ebe2f1f
vinace <vinsent.p@object-frontier.com>
2019-03-27 14:43:19 +0530

    removing string format in config.py

--- configs.py
+++ configs.py
@@ -6,10 +6,10 @@ import pprint
 import os 
 
 project_dir = Path(__file__).resolve().parent
-dataset_dir = str(Path('./data1/jysung710/tmp_sum/360video/').resolve())
+dataset_dir = Path('./data1/jysung710/tmp_sum/360video/').resolve()
 video_list = ['360airballoon', '360parade', '360rowing', '360scuba', '360wedding']
-save_dir = str(Path('./data1/jmcho/SUM_GAN/'))
-score_dir = str(Path('./data1/common_datasets/tmp_sum/360video/results/SUM-GAN/'))
+save_dir = Path('./data1/jmcho/SUM_GAN/')
+score_dir = Path('./data1/common_datasets/tmp_sum/360video/results/SUM-GAN/')
 
 
 def str2bool(v):
